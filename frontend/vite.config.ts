46295e60235c87a124c42fe6171294d4e8c55f2a
igor.ivanov <igor.ivanov@fabrique.studio>
2023-02-21 21:21:36 +0300

    genesis. Добавлен докер

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3001,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./", import.meta.url)),
     },
   },
   plugins: [vue()],
-})
+});
